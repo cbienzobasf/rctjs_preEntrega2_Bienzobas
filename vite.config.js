dd04784397b196a17688c6cf123119c07e61c7c6
Carlos Bienzobas <cbienzobasf@gmail.com>
2025-01-03 15:20:46 -0300

    Ajustes gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rctjs_preEntrega1_Bienzobas/', // Asegúrate de incluir la barra inicial y el nombre exacto del repositorio
+  plugins: [react()],
+  base: 'https://cbienzobasf.github.io/rctjs_preEntrega1_Bienzobas/', // Cambiar según tu repositorio
 });
