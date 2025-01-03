96640ed90c8fc461d8738347a35c01e7b91c84cc
Carlos Bienzobas <cbienzobasf@gmail.com>
2025-01-03 11:47:28 -0300

    Actualización del vite.config.js para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/rctjs_preEntrega1_Bienzobas/', // Asegúrate de incluir la barra inicial y el nombre exacto del repositorio
+});
